76406aec08c1ba8762e393e2b7d75dcd65fba4c9
Alexandra-Shyshkovskaya <74681434+Alexandra-Shyshkovskaya@users.noreply.github.com>
2021-05-04 14:07:57 +0300

    function for open window modal

--- galleryMarkUp.js
+++ galleryMarkUp.js
@@ -25,14 +25,25 @@ function createGalleryMarkUp(gallery) {
     .join('');
 }
 
+/*Функция открытия модального окна с оригинальным изображением*/ 
 function onImageClick(evt) {
 evt.preventDefault();
-  if (!evt.target.classList.contains('gallery__image')) {
-    return
+  if (evt.target.classList.contains('gallery__image')) {
+    const lightbox = document.querySelector('.lightbox');
+    lightbox.classList.add('is-open');
+    lightbox.querySelector('.lightbox__image').src = evt.target.dataset.source;
+    lightbox.querySelector('.lightbox__image').alt = evt.target.alt;
+    
   }
-  console.log(evt.target);
-}
+
+ /* const currentOpenImg = document.querySelector('.gallery__image.is-open');
+  if (currentOpenImg) {
+    currentOpenImg.classList.remove('is-open');
+  }*/
+ /* const lightbox = evt.target;
+  lightbox.classList.add('is-open');
+  lightbox.src = evt.target.dataset.source;
+  console.log(lightbox);*/
 
  
-
-
+}
